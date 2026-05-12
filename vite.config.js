ffe5cdc27a694d3148ebe7245bae1e478891cbff
Yura Davis <yura.davis@gmail.com>
2026-05-12 09:45:46 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/eurovision-2026-scoreboard/',
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 })
