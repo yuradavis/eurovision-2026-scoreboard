c206bdf741ce1a55af3fbc1dd5cd758496254686
Yura Davis <yura.davis@gmail.com>
2026-05-12 09:56:29 +0300

    Fix router for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/globals.css'
 
-// GitHub Pages SPA routing redirect handler
-(function () {
-  const redirect = sessionStorage.redirect
-  delete sessionStorage.redirect
-  if (redirect && redirect !== location.href) {
-    history.replaceState(null, null, redirect)
-  }
-})()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/eurovision-2026-scoreboard">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
